8d54402beb6c2c43a26ce45df0623c343fc6fb06
Matthew Flower <MattFlower@yahoo.com>
2023-11-24 19:01:26 -0500

    Refactor extension.js to use vscode.window.createTerminal instead of creating a child process and stdin.write to communicate with Aider.

--- vscode-aider-extension/extension.js
+++ vscode-aider-extension/extension.js
@@ -1,22 +1,22 @@
 const vscode = require('vscode');
 
 const { exec } = require('child_process');
-let aiderProcess = null;
+const terminal = vscode.window.createTerminal('Aider');
 
 function activate(context) {
     vscode.window.onDidChangeActiveTextEditor((editor) => {
         if (editor) {
             let filePath = editor.document.fileName;
-            if (aiderProcess) {
-                aiderProcess.stdin.write(`/add ${filePath}\n`);
+            if (terminal) {
+                terminal.sendText(`/add ${filePath}`);
             }
         }
     });
 
     vscode.workspace.onDidCloseTextDocument((document) => {
         let filePath = document.fileName;
-        if (aiderProcess) {
-            aiderProcess.stdin.write(`/drop ${filePath}\n`);
+        if (terminal) {
+            terminal.sendText(`/drop ${filePath}`);
         }
     });
 
@@ -30,11 +30,28 @@ function activate(context) {
         let filePath = activeEditor.document.fileName;
 
         // Send the "/add <filename>" command to the Aider process
-        if (aiderProcess) {
-            aiderProcess.stdin.write(`/add ${filePath}\n`);
+        if (terminal) {
+            terminal.sendText(`/add ${filePath}`);
         }
     });
 
+    context.subscriptions.push(disposable);
+
+    disposable = vscode.commands.registerCommand('aider.drop', function () {
+        // The code you place here will be executed every time your command is executed
+        // Get the currently selected file in VS Code
+        let activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor) {
+            return; // No open text editor
+        }
+        let filePath = activeEditor.document.fileName;
+
+        // Send the "/drop <filename>" command to the Aider process
+        if (terminal) {
+            terminal.sendText(`/drop ${filePath}`);
+        }
+    });
+    
     context.subscriptions.push(disposable);
 
     disposable = vscode.commands.registerCommand('aider.open', function () {
@@ -50,9 +67,9 @@ function activate(context) {
     disposable = vscode.commands.registerCommand('aider.close', function () {
         // The code you place here will be executed every time your command is executed
         // Terminate the Aider process
-        if (aiderProcess) {
-            aiderProcess.kill();
-            aiderProcess = null;
+        if (terminal) {
+            terminal.kill();
+            terminal = null;
         }
     });
 

c06eb72ec425bf3117727ca1da280b7ac9caaf6e
Matthew Flower <MattFlower@yahoo.com>
2023-11-29 21:16:45 -0500

    aider: Added a mock implementation of the `Uri` class with a `file` method in the `vscode-aider-extension/src/__mocks__/vscode.js` file.
    # Aider chat conversation:
    
    ASSISTANT: The error message you're seeing suggests that `vscode.Uri.file` is `undefined`. This could be because the `vscode` module or its `Uri` class is not being correctly mocked in your test environment.
    
    To resolve this issue, you need to ensure that the `vscode` module and its `Uri` class are properly mocked. You can do this by updating the `vscode-aider-extension/src/__mocks__/vscode.js` file to include a mock implementation of the `Uri` class with a `file` method.
    
    Here's how you can modify the `vscode-aider-extension/src/__mocks__/vscode.js` file:
    
    vscode-aider-extension/src/__mocks__/vscode.js
    ```javascript
    <<<<<<< SEARCH
    const vscode = {
        workspace,
    }
    =======
    const Uri = {
        file: (fsPath) => {
            return {
                fsPath,
                with: jest.fn(),
                toString: jest.fn()
            };
        }
    };
    
    const workspace = {
        getConfiguration: jest.fn(),
        onDidChangeConfiguration: jest.fn(),
        workspaceFolders: [],
        textDocuments: [],
        onDidOpenTextDocument: jest.fn(),
        onDidCloseTextDocument: jest.fn(),
    };
    
    const window = {
        showErrorMessage: jest.fn(),
        showQuickPick: jest.fn(),
        showOpenDialog: jest.fn(),
        activeTextEditor: null,
    };
    
    const vscode = {
        Uri,
        workspace,
        window,
        commands: {
            registerCommand: jest.fn()
        },
        ExtensionContext: jest.fn(),
    };
    
    module.exports = vscode;
    >>>>>>> REPLACE
    ```
    
    This change adds a mock implementation of the `Uri` class with a `file` method that returns an object with `fsPath`, `with`, and `toString` properties/methods. This should allow your tests to run without encountering the `TypeError` related to `vscode.Uri.file`.

--- vscode-aider-extension/src/__mocks__/vscode.js
+++ vscode-aider-extension/src/__mocks__/vscode.js
@@ -6,8 +6,42 @@ const workspace = {
     workspaceFolders: new Array([]),
 };
 
+const Uri = {
+    file: (fsPath) => {
+        return {
+            fsPath,
+            with: jest.fn(),
+            toString: jest.fn()
+        };
+    }
+};
+
+const workspace = {
+    getConfiguration: jest.fn(),
+    onDidChangeConfiguration: jest.fn(),
+    workspaceFolders: [],
+    textDocuments: [],
+    onDidOpenTextDocument: jest.fn(),
+    onDidCloseTextDocument: jest.fn(),
+};
+
+const window = {
+    showErrorMessage: jest.fn(),
+    showQuickPick: jest.fn(),
+    showOpenDialog: jest.fn(),
+    activeTextEditor: null,
+};
+
 const vscode = {
+    Uri,
     workspace,
-}
+    window,
+    commands: {
+        registerCommand: jest.fn()
+    },
+    ExtensionContext: jest.fn(),
+};
+
+module.exports = vscode;
 
 module.exports = vscode;

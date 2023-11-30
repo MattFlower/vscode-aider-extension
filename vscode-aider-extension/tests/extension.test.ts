6d9b3ec5c8292df1fa9263b78f678cad9d92ae88
Matthew Flower <MattFlower@yahoo.com>
2023-11-29 21:15:15 -0500

    Refactored findWorkspaceDirectory test to use a mock workspaceFolders property.

--- vscode-aider-extension/tests/extension.test.ts
+++ vscode-aider-extension/tests/extension.test.ts
@@ -19,16 +19,14 @@ describe('findWorkingDirectory should support paths from windows and mac', () =>
 describe('findWorkspaceDirectory should support a single workspace folder on windows and mac', () => {
     it ('should find the .git folder in mac', async () => {
         // Mock the workspaceFolders property
-        Object.defineProperty(vscode.workspace, 'workspaceFolders', {
-            get: () => [{
-                uri: vscode.Uri.file("/Users/user/Documents/project/src/"),
-                name: "project",
-                index: 0
-            }]
-        });
+        Object.defineProperty(vscode.workspace, 'workspaceFolders', [{
+            name: "project1",
+            uri: {
+                fsPath: "/Users/user/Documents/project/src/"
+            }
+        }]);
 
-        const path = '/Users/user/Documents/project/src/';
-        const workingDirectory = await findWorkingDirectory(path);
+        const workingDirectory = await findWorkingDirectory('');
         expect(workingDirectory).toEqual('/Users/user/Documents/project/src/');
     });
 });
